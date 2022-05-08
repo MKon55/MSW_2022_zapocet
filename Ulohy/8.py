0b4ff6d86765a4f293ee09b9f3e6884870420113
Michal Konrád <misapost@seznam.cz>
2022-05-08 12:06:42 +0200

    Test
    úlohy 3 a 8

--- Ulohy/8.py
+++ Ulohy/8.py
@@ -1,1 +1,32 @@
 #pass
+
+def forward_derivate(f, x0, h):
+    return (f(x0+h) - f(x0))/h
+
+def backward_derivate(f, x0, h):
+    return (f(x0) - f(x0-h))/h
+
+def central_derivate(f, x0, h):
+    return (f(x0+h) - f(x0-h))/(2*h)
+
+def forward_derivate_adaptive(f, x0, h_adaptive):
+    return (f(x0+h_adaptive) - f(x0))/h_adaptive
+
+def backward_derivate_adaptive(f, x0, h_adaptive):
+    return (f(x0) - f(x0-h_adaptive))/h_adaptive
+
+def central_derivate_adaptive(f, x0, h_adaptive):
+    return (f(x0+h_adaptive) - f(x0-h_adaptive))/(2*h_adaptive)
+
+f = lambda x: x**2 + 2
+x0 = 2
+h = 0.1
+h_adaptive = float(input("Zadejte adaptive krok: "))
+
+print(forward_derivate(f, x0, h))
+print(backward_derivate(f, x0, h))
+print(central_derivate(f, x0, h))
+
+print(forward_derivate_adaptive(f, x0, h_adaptive))
+print(backward_derivate_adaptive(f, x0, h_adaptive))
+print(central_derivate_adaptive(f, x0, h_adaptive))
